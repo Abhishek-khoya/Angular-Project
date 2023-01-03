3e0216f5a40863540a554e962a869d4ea5e95c6d
Abhishek Khoya <abhishek.khoya@yash.com>
2023-01-03 22:31:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
+import { AddDesignationComponent } from './add-designation/add-designation.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ListEmployeesComponent } from './list-employees/list-employees.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ListEmployeesComponent]
 })
 export class AppComponent {
   title = 'HumanResourcesUI';
+  constructor(private dialog:MatDialog,private listOfEmps:ListEmployeesComponent)
+  {}
+  addEmployee()
+  {
+    this.dialog.open(AddEmployeeComponent,{
+      width:'auto',
+      height:'auto'
+    } ).afterClosed().subscribe((val)=>{
+      if(val==='save'){
+        window.location.reload();
+      }
+    })
+  }
+  addDesignation()
+  {
+    this.dialog.open(AddDesignationComponent,{
+      width:'25%',
+      height:'32%'
+    } ).afterClosed().subscribe((val)=>{
+      if(val==='save'){
+        window.location.reload();
+      }
+    })
+  }
+  
 }
